8b175f81018894f19fa7ff100d4acc8faea8a8a5
MHJMBS <mhjmbs@hotmail.com>
2023-02-09 19:18:39 -0300

    Revises all segment tree notes and refactorates some code to be more readable

--- data_structures/segment_tree.cpp
+++ data_structures/segment_tree.cpp
@@ -1,75 +1,68 @@
 /*
-Segment tree is a data structure good for querying for an operation in an array as long as:
-    The operation is associative.
-    The operations possesses a neutral element.
-It also allows for efficient computing of results after a change in the array.
+Segment tree is a data structure that, given a binary associative function f with neutral element:
+    Calculates the value of f in an interval [l,r].
+    Updates the value of a given index i.
+
 Time Complexity:
     Initializing the tree: O(n).
     Query: O(logn)
-    Computing results after changes: O(logn)
-    Where n is the amount of nodes in the tree.
+    Updates: O(logn)
+    Where n is the number of nodes in the tree.
 
-Segtrees value's are stored in a common array, but it's indexing allows for usage as a tree.
-A segtree for the sum of an interval for an array of size 8 looks like this:
+In sake of simplicity of understading, let's consider a segment tree for the sum of an interval.
+
+Lets consider the following array a:
 
 Array:                  2 1 3 6 5 8 4 2
 Array's indexes:        0 1 2 3 4 5 6 7
 
-Segtree:
+Segment Tree sums the numbers two by two, storing the result of each sum as the parent of the two summed numbers.
+Then, it sums the previous results two by two, storing the new result as the parent of the two summed results.
+This goes on until we're left with one result: the sum of the entire array.
+
+For the array a, this is visualized as following:
+
+a's sum segtree:
 
                        31
                    12      19
                  3   9   13  6
                 2 1 3 6 5 8 4 2
 
-Segtree's array indexes:
+All this information is stored in a single array in the following manner:
+
+a's sum segtree array indexes:
 
                         0
                    1          2
                  3   4     5     6
                 7 8 9 10 11 12 13 14
 
-Noticeably:
-    The child's of node of index n are indexed 2*n+1 and 2*n+2.
-    The first index of the leaf's row is array.size()-1.
+This convenient indexing discards the need for the storage of any aditional information for the tree traversal, given that, for a non-leaf node in index i:
+    The node at 2*i+1 is it's left child.
+    The node at 2*i+2 is it's right child.
 
-The segtree is a perfect binary tree, so every non-leaf node has exactly two childs.
-This means the leaf row of the tree can't be exactly the initial array, because it's size may not be a power of two.
-The remaining nodes are set to the neutral element of the operation we want our segtree to efficiently compute in an interval.
-This way they don't interfere with other results. 
+Notice that, as every node has two childs except for leafs, a segment tree is a perfect binary tree.
+Given that, for a perfect binary tree to be possible, every level, from the root level to the leaf level, must have a power of two number of elements.
+As the original array's number of elements may not be a power of two, these remaining spots may interfere in the overall result of some node.
+This can be solved by filling the extra spots of the leaf row with the neutral element of the operation being considered.
 
-For it's proper usage, segtree requires for 3 basic functions: init, set and query.
+Segtree's functionality is separated in three functions:
 
 Init:
-    The init function initializes the segtree array, it must be called before everythin else and only once.
-    It finds the smallest power of two bigger than the initial array and sets the segtree array that size.
-    For an initial array of size n, all n initial positions of the leaf's row are set equal to the initial array.
-    The remaining elements are set as the neutral element of this segtree's operation.
-    All non-leaf nodes are set to be the result of the operation of it's two children.
-Set:
-    The set function makes changes to a leaf node the segtree array and updates all it's parents.
+    The init function takes an array and builds it's segtree for some operation.
+        It finds the smallest power of two x bigger than the initial array and sets the segtree array size to be 2x-1.
+        For an initial array of size n, all n initial positions of the leaf's row are set equal to the initial array. *The index of the first element in the leaf row is the array size - x.
+        The remaining elements are set as the neutral element of this segtree's operation.
+        All non-leaf nodes are set to be the result of the operation of it's two children.
+
+Update:
+    The update function makes changes to a leaf node the segtree array and updates all it's parents, navigating through the indexes by their mentioned properties.
 Query:
-    The query function returns the result of the operation in a specific interval of the array.
+    The query function returns the result of the operation given a specific interval [l,r] of the array.
 
 All three functions may be recursively or iteratively implemented.
-The init and set iterative functions are faster and more easily implemented than their recursive alternatives.
-As for the query, the iterative implementation is really complex, so the recursive is a better choice, even being slower.
-
-The funcionts are implemented using the following property:
-    The child's of node of index n are indexed 2*n+1 and 2*n+2.
-
-Init and set are implemented straight-fowardly from the property above, with a bottom up approach.
-
-Query is implemented with a top down recursive approach, with the following cases:
-If current node perfectly represent any part from the desired interval, return it's value.
-If it perfectly does not represent any part from the desired interval, return the neutral element of the operation.
-If it partially represents the desired interval, return query(2*n+1) operation query(2*n+2).
-
-The intervals represented by each node are calculated as query is called.
-    Index 0 represents the whole leaf row.
-    Index 1 represents [0, (leafRowSize-1)/2]
-    Index 2 represents [(leafRowSize-1)/2 + 1 , (leafRowSize-1)]
-And this logic goes on for every node.
+In the following examples, Init and Update are iterative, whilst Query is recursive.
 */
 
 
@@ -82,120 +75,98 @@ using namespace std;
 using ll = long long;
 using pii = pair<int,int>;
 
-//Segtree for the sum of an interval
+//Segtree for the sum of values in an interval
 
-struct segtree {
-    int lrStart;
-    int lrSize = 1;
-    vector<ll> st;
+struct SegTree {
+    vector<ll> seg;
+    int lr_start; //lr refers to the last row of the tree, i.e, the row composed by all it's leafs.
+    int lr_size = 1;
 
-    segtree(vector<int>& a) {
+    SegTree(vector<int> &a) {
         setSize(a.size());
-        lrStart = st.size() - lrSize;
 
-        for(int i = lrStart; i - lrStart < a.size(); i++) {
-            st[i] = a[i - lrStart];
+        lr_start = seg.size() - lr_size;
+
+        for(int i = 0; i < a.size(); i++) {
+            seg[lr_start + i] = a[i];
         }
 
-        for(int i = lrStart-1; i >= 0; i--) {
-            st[i] = st[2*i+1] + st[2*i+2];
+        for(int i = lr_start-1; 0 <= i; i--) {
+            seg[i] = seg[2*i+1] + seg[2*i+2];
         }
     }
 
-    void setSize(int initSize) {
-        while(lrSize < initSize) {
-            lrSize *= 2;
-        }
-
-        st.resize(2*lrSize - 1, 0);
+    void setSize(int array_size) {
+        while(lr_size < array_size) lr_size *= 2;
+        seg.resize(2*lr_size - 1, 0); //all positions intialized to the operation's neutral element.
     }
 
     void set(int i, int v) {
-        i = lrStart + i;
-        st[i] = v;
+        i = lr_start + i;
+        seg[i] = v;
 
         while(i > 0) {
-            if(i % 2 == 0) {
-                st[(i-2)/2] = st[i] + st[i-1];
-                i = (i-2)/2;
-            }else {
-                st[(i-1)/2] = st[i] + st[i+1];
-                i = (i-1)/2;
-            }
+            if(i % 2 == 0) i = (i-2)/2;
+            else i = (i-1)/2;
+            seg[i] = seg[2*i+1] + seg[2*i+2];
         }
     }
 
     ll query(int l, int r, int lx = 0, int rx = -1, int i = 0) {
-        if(rx == -1) {
-            rx = lrSize-1;
-        }
+        if(rx == -1) rx = lr_size-1;
 
-        if(l <= lx && rx <= r) {
-            return st[i];
-        }else if(rx < l || r < lx) {
-            return 0;
-        }else {
-            int m = lx + (rx-lx)/2;
-            return query(l, r, lx, m, 2*i+1) + query(l, r, m+1, rx, 2*i+2);
-        }
+        int m = (lx+rx)/2;
+
+        if(rx < l || r < lx) return 0;
+        if(l <= lx && rx <= r) return seg[i];
+        return query(l, r, lx, m, 2*i+1) + query(l, r, m+1, rx, 2*i+2);
     }
 };
 
-//Segtree for the minimum in an interval
+//Segtree for the minimum value in an interval
 
-struct segtree {
-    int lrStart;
-    int lrSize = 1;
-    vector<int> st;
+struct SegTree {
+    vector<int> seg;
+    int lr_start; //lr refers to the last row of the tree, i.e, the row composed by all it's leafs.
+    int lr_size = 1;
 
-    segtree(vector<int>& a) {
+    SegTree(vector<int> &a) {
         setSize(a.size());
-        lrStart = st.size() - lrSize;
 
-        for(int i = lrStart; i - lrStart < a.size(); i++) {
-            st[i] = a[i - lrStart];
+        lr_start = seg.size() - lr_size;
+
+        for(int i = 0; i < a.size(); i++) {
+            seg[lr_start + i] = a[i];
         }
 
-        for(int i = lrStart-1; i >= 0; i--) {
-            st[i] = min(st[2*i+1], st[2*i+2]);
+        for(int i = lr_start-1; 0 <= i; i--) {
+            seg[i] = min(seg[2*i+1], seg[2*i+2]);
         }
     }
 
-    void setSize(int initSize) {
-        while(lrSize < initSize) {
-            lrSize *= 2;
-        }
-
-        st.resize(2*lrSize - 1, INT_MAX);
+    void setSize(int array_size) {
+        while(lr_size < array_size) lr_size *= 2;
+        seg.resize(2*lr_size - 1, INT_MAX); //all positions intialized to the operation's neutral element.
     }
 
     void set(int i, int v) {
-        i = lrStart + i;
-        st[i] = v;
+        i = lr_start + i;
+        seg[i] = v;
 
         while(i > 0) {
-            if(i % 2 == 0) {
-                st[(i-2)/2] = min(st[i], st[i-1]);
-                i = (i-2)/2;
-            }else {
-                st[(i-1)/2] = min(st[i], st[i+1]);
-                i = (i-1)/2;
-            }
+            if(i % 2 == 0) i = (i-2)/2;
+            else i = (i-1)/2;
+            seg[i] = min(seg[2*i+1], seg[2*i+2]);
         }
     }
 
-    int query(int l, int r, int lx = 0, int rx = -1, int i = 0) {
-        if(rx == -1) {
-            rx = lrSize-1;
-        }
+    ll query(int l, int r, int lx = 0, int rx = -1, int i = 0) {
+        if(rx == -1) rx = lr_size-1;
 
-        if(l <= lx && rx <= r) {
-            return st[i];
-        }else if(rx < l || r < lx) {
-            return INT_MAX;
-        }else {
-            int m = lx + (rx-lx)/2;
-            return min(query(l, r, lx, m, 2*i+1), query(l, r, m+1, rx, 2*i+2));
-        }
+        int m = (lx+rx)/2;
+
+        if(rx < l || r < lx) return 0;
+        if(l <= lx && rx <= r) return seg[i];
+        return min(query(l, r, lx, m, 2*i+1), query(l, r, m+1, rx, 2*i+2));
     }
 };
